8377c91f5d79134e2a3df6b7f30a40f0e8bac38e
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-07-04 11:59:15 +0200

    make debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@ export PYTHONUNBUFFERED=0
 
 ls -tlroa /var/log/containers/
 
-python /cdci_data_analysis/bin/run_osa_cdci_server.py -conf_file /dispatcher/conf/conf_env.yml -use_gunicorn 2>&1 | tee -a /var/log/containers/${CONTAINER_NAME}
+python /cdci_data_analysis/bin/run_osa_cdci_server.py \
+    -conf_file /dispatcher/conf/conf_env.yml \
+    -debug \
+    -use_gunicorn 2>&1 | tee -a /var/log/containers/${CONTAINER_NAME} -
